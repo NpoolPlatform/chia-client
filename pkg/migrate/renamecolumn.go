cab5db8b6290f3112a794e30861e6ec09b6c0886
Zhao KK <kikakkz@hotmail.com>
2022-08-01 07:52:01 +0000

    Better rename column

--- pkg/migrate/renamecolumn.go
+++ pkg/migrate/renamecolumn.go
@@ -6,7 +6,7 @@ import (
 	"entgo.io/ent/dialect/sql"
 )
 
-func RenameColumn(ctx context.Context, conn dialect.ExecQuerier, table, srcColumn, dstColumn, columnType string, unsigned bool) error {
+func tableExist(ctx context.Context, conn dialect.ExecQuerier, table string) (bool, error) {
 	rows := sql.Rows{}
 
 	query, args := sql.
@@ -20,54 +20,74 @@ func RenameColumn(ctx context.Context, conn dialect.ExecQuerier, table, srcColum
 		Count().
 		Query()
 	if err := conn.Query(ctx, query, args, &rows); err != nil {
-		return err
+		return false, err
 	}
 
 	for rows.Next() {
 		count := 0
 		if err := rows.Scan(&count); err != nil {
-			return err
+			return false, err
 		}
 		if count == 0 {
 			rows.Close()
-			return nil
+			return false, nil
 		}
 	}
 	rows.Close()
 
-	query, args = sql.
+	return true, nil
+}
+
+func columnExist(ctx context.Context, conn dialect.ExecQuerier, table, column string) (bool, error) {
+	rows := sql.Rows{}
+
+	query, args := sql.
 		Select("datetime_precision").
 		From(sql.Table("`information_schema`.`columns`")).
 		Where(
 			sql.And(
 				sql.EQ("table_name", table),
-				sql.EQ("column_name", dstColumn),
+				sql.EQ("column_name", column),
 			),
 		).
 		Count().
 		Query()
 
 	if err := conn.Query(ctx, query, args, &rows); err != nil {
-		return err
+		return false, err
 	}
 
 	for rows.Next() {
 		count := 0
 		if err := rows.Scan(&count); err != nil {
-			return err
+			return false, err
 		}
 		if count > 0 {
 			rows.Close()
-			return nil
+			return true, nil
 		}
 	}
 	rows.Close()
+
+	return false, nil
+}
+
+func RenameColumn(ctx context.Context, conn dialect.ExecQuerier, table, srcColumn, dstColumn, columnType string, unsigned bool) error {
+	if exist, err := tableExist(ctx, conn, table); err != nil || !exist {
+		return err
+	}
+	if exist, err := columnExist(ctx, conn, table, srcColumn); err != nil || !exist {
+		return err
+	}
+	if exist, err := columnExist(ctx, conn, table, dstColumn); err != nil || exist {
+		return err
+	}
 
 	cb := sql.
 		Column(dstColumn).
 		Type(columnType)
 
-	query, args = sql.
+	query, args := sql.
 		AlterTable(table).
 		ChangeColumn(srcColumn, cb).
 		Query()

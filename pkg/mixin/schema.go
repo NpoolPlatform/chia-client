ad623f995ed21eb78eabc848b660440e8351bf83
Zhao KK <kikakkz@hotmail.com>
2023-05-06 05:15:39 +0000

    Add auto id to mixin

--- pkg/mixin/schema.go
+++ pkg/mixin/schema.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
 	"entgo.io/ent/schema/field"
 	"entgo.io/ent/schema/mixin"
 )
@@ -17,19 +18,30 @@ type Policy interface {
 }
 
 func (Schema) Fields() []ent.Field {
+	incrementalEnabled := true
 	return []ent.Field{
-		field.Uint32("created_at").
+		field.
+			Uint32("auto_id").
+			Unique().
+			Annotations(
+				entsql.Annotation{
+					Incremental: &incrementalEnabled,
+				}),
+		field.
+			Uint32("created_at").
 			DefaultFunc(func() uint32 {
 				return uint32(time.Now().Unix())
 			}),
-		field.Uint32("updated_at").
+		field.
+			Uint32("updated_at").
 			DefaultFunc(func() uint32 {
 				return uint32(time.Now().Unix())
 			}).
 			UpdateDefault(func() uint32 {
 				return uint32(time.Now().Unix())
 			}),
-		field.Uint32("deleted_at").
+		field.
+			Uint32("deleted_at").
 			DefaultFunc(func() uint32 {
 				return 0
 			}),

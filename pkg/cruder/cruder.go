04358a08a9f6ffdc1615bbdaea7c24b6f839be12
Zhao KK <kikakkz@hotmail.com>
2023-08-25 07:35:58 +0000

    Add n between

--- pkg/cruder/cruder.go
+++ pkg/cruder/cruder.go
@@ -3,16 +3,17 @@ package cruder
 type Any interface{}
 
 const (
-	EQ      = "eq"
-	NEQ     = "neq"
-	GT      = "gt"
-	GTE     = "gte"
-	LT      = "lt"
-	LTE     = "lte"
-	IN      = "in"
-	LIKE    = "like"
-	NIN     = "nin"
-	BETWEEN = "between"
+	EQ       = "eq"
+	NEQ      = "neq"
+	GT       = "gt"
+	GTE      = "gte"
+	LT       = "lt"
+	LTE      = "lte"
+	IN       = "in"
+	LIKE     = "like"
+	NIN      = "nin"
+	BETWEEN  = "between"
+	NBETWEEN = "nbetween"
 )
 
 type Cond struct {

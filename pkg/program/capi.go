cf0d48677dbd106d26b435329b3f052a63c901da
daqiangli <dqli161@163.com>
2024-09-01 22:36:42 +0800

    add go rust demo

---
+++ pkg/program/capi.go
@@ -0,0 +1,5 @@
+package native
+
+// #cgo LDFLAGS: -L./capi/libs -lchia_capi_x86_64-unknown-linux-musl
+// #include <stdlib.h>
+// #include "capi/include/chia_capi.h"
---
+++ pkg/program/capi_linux_amd64.go
@@ -0,0 +1,5 @@
+package native
+
+// #cgo LDFLAGS: -L./capi/libs -lchia_capi_x86_64-apple-darwin20
+// #include <stdlib.h>
+// #include "capi/include/chia_capi.h"
